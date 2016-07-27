cfb33a4b0fc2a770240282483f2042972b38bbb2
rmody <rmody@Ravis-MacBook-Air.local>
2016-07-27 00:43:16 -0400

    wrapped into a loop. write to a pandas df and tsv file

--- tag_episodes/tag_simpsons_submissions.py
+++ tag_episodes/tag_simpsons_submissions.py
@@ -6,25 +6,54 @@ es = elasticsearch.Elasticsearch()
 with open('simpsons_submissions.txt') as f:
     submission_ids = f.read().splitlines()
     
-submission_id = submission_ids[1]
-submission = scraper.get_submission(submission_id=submission_id)
+    
+episode_df = pd.DataFrame(columns= [
+    'submission_id', 'submission_created', 'submission_url', 'submission_permalink', 'submission_is_self',
+    'submission_ups', 'submission_downs',
+    'comments_ep', 'comments_score', 'num_comments',
+    'title_ep', 'title_score',
+    'flair_text'
+    ]
+)
 
-# get sum of scores of all comments by episode
-scores = {}
-for comment in submission.comments:
-    search_result = get_episode(comment.body)
-    print search_result
-    if search_result['score']:
-        if search_result['episode'] in scores:
-            scores[search_result['episode']] += search_result['score']
-        else:
-            scores[search_result['episode']] = search_result['score']
+for submission_id in submission_ids:
+    submission = scraper.get_submission(submission_id=submission_id)
 
-# get episode from title
-get_episode(submission.title)        
+    # get sum of scores of all comments by episode
+    scores = {}
+    for comment in submission.comments:
+        search_result = get_episode(comment.body)
+        if search_result['score']:
+            if search_result['episode'] in scores:
+                scores[search_result['episode']] += search_result['score']
+            else:
+                scores[search_result['episode']] = search_result['score']
 
-# get flair text
-submission.link_flair_text
+    num_comments = len(submission.comments)
+    if scores:
+        comments_ep = max(scores, key=scores.get)
+        comments_score = scores[comments_ep]
+    else:
+        comments_ep = None
+        comments_score = None
+
+    # get episode from title
+    title = get_episode(submission.title)
+    title_ep = title['episode']
+    title_score = title['score']
+
+    # get flair text
+    flair_text = submission.link_flair_text
+
+    episode_df.loc[len(episode_df)]=[
+        submission_id, submission.created, submission.url, submission.permalink, submission.is_self,
+        submission.ups, submission.downs,
+        comments_ep, comments_score, num_comments,
+        title_ep, title_score,
+        flair_text
+    ]
+    print len(episode_df.index)
+
 
 # function to get episode from searching transcripts
 def get_episode(text):
